1c918ebd53c08d3a6a7522b94c81b76f8f287e7a
theoderic <onipetheoderic@gmail.com>
2022-06-21 15:16:05 +0200

    Initial Commit

---
+++ Shared/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  Shared
+//
+//  Created by Theoderic onipe on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
